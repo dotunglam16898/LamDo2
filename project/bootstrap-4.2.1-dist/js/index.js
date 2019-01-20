205b363e2cda79fcc005c18094192d4fbf80d2e5
dotunglam16898 <45615863+dotunglam16898@users.noreply.github.com>
2019-01-20 15:46:17 +0700

    Add files via upload

--- project/bootstrap-4.2.1-dist/js/index.js
+++ project/bootstrap-4.2.1-dist/js/index.js
@@ -1,43 +1,13 @@
-$(window).scroll(function () {
-    var top = $(document).scrollTop();
-    var height = 300;
-    if (top > height) {
-        $('.navbar-fixed-top').addClass('menu-scroll');
-    } else {
-        $('.navbar-fixed-top').removeClass('menu-scroll');
-    }
-});
-// $(window).load(function () {
-//         $(".loaded").fadeOut();
-//         $(".preloader").delay(1000).fadeOut("slow");
-//     });
-// 
-// // scroll Up
-
-//     $(window).scroll(function () {
-//         if ($(this).scrollTop() > 600) {
-//             $('.scrollup').fadeIn('slow');
-//         } else {
-//             $('.scrollup').fadeOut('slow');
-//         }
-//     });
-
-//     $('.scrollup').click(function () {
-//         $("html, body").animate({scrollTop: 0}, 1000);
-//         return false;
-//     });         
 // $(window).scroll(function () {
-//         if ($(this).scrollTop() > 600) {
-//             $('.scrollup').fadeIn('slow');
-//         } else {
-//             $('.scrollup').fadeOut('slow');
-//         }
-//     });
+//     var top = $(document).scrollTop();
+//     var height = 300;
+//     if (top > height) {
+//         $('.navbar-fixed-top').addClass('menu-scroll');
+//     } else {
+//         $('.navbar-fixed-top').removeClass('menu-scroll');
+//     }
+// });
 
-//     $('.scrollup').click(function () {
-//         $("html, body").animate({scrollTop: 0}, 1000);
-//         return false;
-//     });     
 
 $(function() {
   //Simple filter controls
@@ -60,10 +30,13 @@ $(function() {
   });
   
 });
-// $(document).on("scroll", function () {
-//     if ($(document).scrollTop() > 120) {
-//         $("nav").addClass("smaller");
-//     } else {
-//         $("nav").removeClass("smaller");
-//     }
-// });
+
+function showPage(){
+  $(".preload").hide();
+  $(".showPage").show();
+}
+
+$(window).load(function(){
+  var myTime = setTimeout(showPage,2000); 
+});
+
